23fce443624701c74f02e2f3301c9e3100abe38b
Marc Lenart <mlenart@me.com>
2016-03-21 20:37:42 -0400

    Initialize repo

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var PORT = 3000;		// when a variable name in js is capitalized it is saying not to be changed
+
+var app = express();
+// middleware
+var middleware = {
+	requireAuthentication: function (req, res, next){
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req, res, next){
+		console.log('Request: '+ new Date().toString()+ ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+//app.use - application level middleware
+app.use(middleware.logger);
+
+
+//first parameter is the route (in this example we are indicating root url)
+//req = request - any data passed along - could be cookies...
+//res - data to send back
+//to use route middleware use as the second paramter
+app.get('/about', middleware.requireAuthentication , function(req, res){
+	res.send("All About Us");
+});
+
+//alllow access to a folder
+// __dirname   -  gets filepath from your machine
+app.use(express.static(__dirname + '/public'));
+
+
+//tell the app to listen
+//first aramter is the port to listen on
+//second parameter is the function called after starting of the server
+app.listen(PORT, function(){
+	console.log('Express Server Started on port: ' + PORT);
+});
